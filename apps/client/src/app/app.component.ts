177a2adb8b39015ff3a9ac740c393a350c69a9b2
Usama Ansari <usamazansari@outlook.com>
2022-12-27 22:28:06 +0100

    feat: ✨ added ng-fire

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,14 +1,79 @@
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from '@angular/fire/compat/firestore';
 import { RouterModule } from '@angular/router';
-import { Component } from '@angular/core';
+import {
+  catchError,
+  from,
+  Observable,
+  switchMap,
+  take,
+  throwError
+} from 'rxjs';
+
+type DataType = {
+  id: string;
+  property: string;
+};
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [CommonModule, RouterModule],
   selector: 'expenses-tracker-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  collectionRef!: AngularFirestoreCollection<DataType>;
+  data!: Observable<DataType[]>;
+
+  mock = ['foo', 'bar', 'lorem', 'ipsum'];
+
+  constructor(private _firestore: AngularFirestore) {}
+
+  ngOnInit() {
+    this.collectionRef = this._firestore.collection<DataType>('dummy');
+    this.data = this.collectionRef.valueChanges();
+  }
+
+  addData() {
+    this.collectionRef.add({
+      id: this._firestore.createId(),
+      property: this.mock.at(Math.floor(Math.random() * this.mock.length)) ?? ''
+    });
+  }
+
+  removeFirst() {
+    this.data
+      .pipe(
+        switchMap(([{ id }]) =>
+          this.collectionRef.ref.where('id', '==', id).get()
+        ),
+        switchMap(({ empty, docs }) => {
+          if (!empty) {
+            const [
+              {
+                ref: { id }
+              }
+            ] = docs;
+            return from(this.collectionRef.doc(id).delete());
+          }
+          throw new Error('Document not found');
+        }),
+        catchError(err => throwError(err)),
+        take(1)
+      )
+      .subscribe({
+        next: d => {
+          console.log({ d });
+        },
+        error: err => {
+          console.log({ err });
+        }
+      });
+  }
 }
--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,11 +1,32 @@
+import { importProvidersFrom } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation
 } from '@angular/router';
+
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())]
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    importProvidersFrom(
+      AngularFireModule.initializeApp({
+        projectId: 'ua-expenses-tracker',
+        appId: '1:996752641056:web:c4ab663b5a61a85b264045',
+        storageBucket: 'ua-expenses-tracker.appspot.com',
+        apiKey: 'AIzaSyDWMOv0m9vA37Uk7bUClfcB6dtJ0VwVv84',
+        authDomain: 'ua-expenses-tracker.firebaseapp.com',
+        messagingSenderId: '996752641056',
+        measurementId: 'G-ZM3ZXXJKQ0'
+      }),
+      AngularFireAuthModule,
+      AngularFirestoreModule
+    )
+  ]
 }).catch(err => console.error(err));
