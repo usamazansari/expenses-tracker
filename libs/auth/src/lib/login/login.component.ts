432b5cf522cceaa28f33e14c78b8bde6588b933b
Usama Ansari <usamazansari@outlook.com>
2023-01-06 19:36:34 +0100

    feat: 🎨 implemented UI for login signup flows

--- libs/auth/src/lib/login/login.component.ts
+++ libs/auth/src/lib/login/login.component.ts
@@ -1,8 +1,21 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { BehaviorSubject, debounceTime } from 'rxjs';
 
 import { LoginGraphicComponent } from '@expenses-tracker/shared/assets';
+
+type LoginForm = {
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+};
 
 @Component({
   selector: 'expenses-tracker-login',
@@ -11,8 +24,57 @@ import { LoginGraphicComponent } from '@expenses-tracker/shared/assets';
     CommonModule,
     NgOptimizedImage,
     LoginGraphicComponent,
+    ReactiveFormsModule,
     MatIconModule
   ],
   templateUrl: './login.component.html'
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  #formErrors$ = new BehaviorSubject<string[]>([]);
+  formGroup!: FormGroup<LoginForm>;
+  formErrors: string[] = [];
+
+  constructor(private _fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.formGroup = this._fb.group<LoginForm>({
+      email: this._fb.control<string>('', {
+        validators: [Validators.required, Validators.email]
+      }),
+      password: this._fb.control<string>('', {
+        validators: [Validators.required]
+      })
+    });
+
+    this.formGroup.valueChanges.pipe(debounceTime(500)).subscribe(() => {
+      this.checkErrors();
+    });
+
+    this.#formErrors$.subscribe(e => {
+      this.formErrors = [...e];
+    });
+  }
+
+  login() {
+    if (this.formGroup.valid) {
+      console.log('Logging in with', this.formGroup.value);
+    } else {
+      this.checkErrors();
+    }
+  }
+
+  private checkErrors() {
+    this.#formErrors$.next([]);
+    if (this.formGroup.invalid) {
+      for (const controlName in this.formGroup.controls) {
+        const control = this.formGroup.get(controlName);
+        for (const error in control?.errors) {
+          this.#formErrors$.next([
+            ...this.formErrors,
+            `Error in field ${controlName} - ${error}`
+          ]);
+        }
+      }
+    }
+  }
+}
--- libs/auth/src/lib/signup/signup.component.ts
+++ libs/auth/src/lib/signup/signup.component.ts
@@ -1,7 +1,21 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  ReactiveFormsModule,
+  FormGroup,
+  FormBuilder,
+  Validators
+} from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { BehaviorSubject, debounceTime } from 'rxjs';
+
 import { RegisterGraphicComponent } from '@expenses-tracker/shared/assets';
+
+type SignupForm = {
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+};
 
 @Component({
   selector: 'expenses-tracker-signup',
@@ -10,8 +24,57 @@ import { RegisterGraphicComponent } from '@expenses-tracker/shared/assets';
     CommonModule,
     NgOptimizedImage,
     RegisterGraphicComponent,
+    ReactiveFormsModule,
     MatIconModule
   ],
   templateUrl: './signup.component.html'
 })
-export class SignupComponent {}
+export class SignupComponent {
+  #formErrors$ = new BehaviorSubject<string[]>([]);
+  formGroup!: FormGroup<SignupForm>;
+  formErrors: string[] = [];
+
+  constructor(private _fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.formGroup = this._fb.group<SignupForm>({
+      email: this._fb.control<string>('', {
+        validators: [Validators.required, Validators.email]
+      }),
+      password: this._fb.control<string>('', {
+        validators: [Validators.required]
+      })
+    });
+
+    this.formGroup.valueChanges.pipe(debounceTime(500)).subscribe(() => {
+      this.checkErrors();
+    });
+
+    this.#formErrors$.subscribe(e => {
+      this.formErrors = [...e];
+    });
+  }
+
+  signup() {
+    if (this.formGroup.valid) {
+      console.log('Registering with', this.formGroup.value);
+    } else {
+      this.checkErrors();
+    }
+  }
+
+  private checkErrors() {
+    this.#formErrors$.next([]);
+    if (this.formGroup.invalid) {
+      for (const controlName in this.formGroup.controls) {
+        const control = this.formGroup.get(controlName);
+        for (const error in control?.errors) {
+          this.#formErrors$.next([
+            ...this.formErrors,
+            `Error in field ${controlName} - ${error}`
+          ]);
+        }
+      }
+    }
+  }
+}
