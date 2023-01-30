ee763febe8c1eb8c8f7c62e8a8b690ce791c382d
Usama Ansari <usamazansari@outlook.com>
2023-01-30 17:27:25 +0100

    fix: ⚡ minor fixes in the auth page - 🩹 fixed auth error - 🔥 removed custom interface implementation for User

--- libs/layout/src/lib/components/navbar/navbar.service.ts
+++ libs/layout/src/lib/components/navbar/navbar.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
+import { User } from 'firebase/auth';
 import { BehaviorSubject } from 'rxjs';
 
 import { AuthService } from '@expenses-tracker/auth';
-import { IUser } from '@expenses-tracker/shared/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavbarService {
-  #user$ = new BehaviorSubject<IUser | null>(null);
+  #user$ = new BehaviorSubject<User | null>(null);
 
   constructor(private _authService: AuthService) {
     this._authService.getUser$().subscribe(user => {
@@ -16,7 +16,7 @@ export class NavbarService {
     });
   }
 
-  setUser(user: IUser | null) {
+  setUser(user: User | null) {
     this.#user$.next(user);
   }
 
