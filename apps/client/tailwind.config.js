dbeea368ef116cb06c276140881b6c2386ae8eee
Usama Ansari <usamazansari@outlook.com>
2023-03-11 17:01:43 +0100

    Revert "feat: added dark-variations"
    This reverts commit ab2bbec6c5016510bda14ad51e9f57e0f8ea46c8.

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,6 +1,6 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
 const { join } = require('path');
-const { blue, red, green, gray } = require('tailwindcss/colors');
+const { blue, red, green } = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -13,11 +13,7 @@ module.exports = {
       colors: {
         primary: blue[700],
         secondary: green[500],
-        danger: red[500],
-        dark: gray[700],
-        light: gray[100],
-        'disabled-dark': gray[200],
-        'disabled-light': gray[400]
+        danger: red[500]
       }
     }
   },
