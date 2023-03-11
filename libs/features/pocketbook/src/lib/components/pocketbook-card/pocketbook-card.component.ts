9a231a9c9916028b49ea5ff71dc2e644a9900744
Usama Ansari <98811057+usamazansari@users.noreply.github.com>
2023-03-12 00:00:04 +0530

    feat: implement dark theme (#18)
    * feat: add primary, secondary and danger colors
    
    * feat: added dark-variations
    
    * feat: completed dark theme implementation
    WIP: add switch to toggle theme
    
    * style: strip all colors and add theme colors in config
    
    * style: implement github theme
    
    wip: implement for profile and transaction page
    wip: later cherry pick theme commits and push them in a separate branch
    wip: implement feature to toggle between themes
    
    * style: add dark theme to remaining components
    
    * chore: standardize graphic components code base
    
    * style: add proper colors for pocketbook-add component
    
    * style: implement github colors in standardized tw config
    
    wip: implement for pocketbook components
    
    * style: migrated to github theme
    
    wip: implement consisted cancel button everywhere

---
+++ libs/features/pocketbook/src/lib/components/pocketbook-card/pocketbook-card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'expenses-tracker-pocketbook-card',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  template: `<div class="relative p-4 border rounded-lg min-h-[50vh]">
+    <router-outlet></router-outlet>
+  </div>`,
+  styles: []
+})
+export class PocketbookCardComponent {}
--- libs/pocketbook/src/lib/components/pocketbook-list-item/pocketbook-delete-dialog.component.ts
+++ libs/pocketbook/src/lib/components/pocketbook-list-item/pocketbook-delete-dialog.component.ts
@@ -3,35 +3,51 @@ import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRippleModule } from '@angular/material/core';
+import { DeletePocketbookGraphicComponent } from '@expenses-tracker/shared/assets';
 
 @Component({
   selector: 'expenses-tracker-pocketbook-delete-dialog',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatIconModule, MatRippleModule],
-  template: `<h2 mat-dialog-title style="font-family: unset;">Delete Pocketbook</h2>
-    <mat-dialog-content style="font-family: unset;">
-      Are you sure you want to delete the pocketbook?
-    </mat-dialog-content>
-    <mat-dialog-actions style="display: flex; flex-direction: column;">
-      <div class="flex items-center justify-center gap-6">
-        <button
-          matRipple
-          [mat-dialog-close]="true"
-          type="button"
-          class="flex items-center w-full gap-2 px-6 py-3 text-center text-white bg-red-500 rounded-lg cursor-pointer font-bold">
-          <mat-icon class="min-w-[24px]">delete</mat-icon>
-          <span>YES</span>
-        </button>
-        <button
-          matRipple
-          mat-dialog-close
-          type="button"
-          class="flex items-center w-full gap-2 px-6 py-3 text-center text-blue-700 rounded-lg cursor-pointer hover:underline hover:underline-offset-4">
-          <mat-icon class="min-w-[24px]">clear</mat-icon>
-          <span>NO</span>
-        </button>
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatIconModule,
+    MatRippleModule,
+    DeletePocketbookGraphicComponent
+  ],
+  template: `
+    <div
+      class="grid p-6 gap-6 rounded-lg border bg-et-layer-alternate text-et-color-fg-default border-et-color-button-default-border">
+      <div class="flex items-center gap-2">
+        <expenses-tracker-delete-pocketbook-graphic
+          class="w-20 fill-et-color-fg-danger"></expenses-tracker-delete-pocketbook-graphic>
+        <p class="text-3xl text-et-color-fg-danger">Delete Pocketbook</p>
       </div>
-    </mat-dialog-actions>`,
+      <div class="">
+        <p class="">Are you sure you want to delete the pocketbook?</p>
+      </div>
+      <div class="flex flex-col">
+        <div class="flex items-center justify-center gap-6">
+          <button
+            matRipple
+            [mat-dialog-close]="true"
+            type="button"
+            class="flex justify-center items-center w-full gap-2 px-6 py-3 text-center text-et-color-button-danger bg-et-color-button-danger-bg rounded-lg cursor-pointer font-bold">
+            <mat-icon class="min-w-[24px]">delete</mat-icon>
+            <span>YES</span>
+          </button>
+          <button
+            matRipple
+            mat-dialog-close
+            type="button"
+            class="flex justify-center items-center w-full gap-2 px-6 py-3 text-center rounded-lg cursor-pointer hover:underline hover:underline-offset-4 text-et-color-button-default bg-et-color-button-default-bg hover:text-et-color-button-accent">
+            <mat-icon class="min-w-[24px]">clear</mat-icon>
+            <span>NO</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  `,
   styles: []
 })
 export class PocketbookDeleteDialogComponent {}
