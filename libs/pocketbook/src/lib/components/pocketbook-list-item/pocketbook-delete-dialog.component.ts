e8bf1d1fc4d597f0e23c1f7125af5567c0c3524c
Usama Ansari <usamazansari@outlook.com>
2023-03-11 19:26:08 +0100

    style: migrated to github theme
    wip: implement consisted cancel button everywhere

--- libs/pocketbook/src/lib/components/pocketbook-list-item/pocketbook-delete-dialog.component.ts
+++ libs/pocketbook/src/lib/components/pocketbook-list-item/pocketbook-delete-dialog.component.ts
@@ -16,14 +16,15 @@ import { DeletePocketbookGraphicComponent } from '@expenses-tracker/shared/asset
     DeletePocketbookGraphicComponent
   ],
   template: `
-    <div class="grid p-6 gap-6 rounded border bg-et-layer-alternate">
+    <div
+      class="grid p-6 gap-6 rounded-lg border bg-et-layer-alternate text-et-color-fg-default border-et-color-button-default-border">
       <div class="flex items-center gap-2">
         <expenses-tracker-delete-pocketbook-graphic
-          class="w-20 fill-et-danger"></expenses-tracker-delete-pocketbook-graphic>
-        <p class="text-3xl text-et-danger">Delete Pocketbook</p>
+          class="w-20 fill-et-color-fg-danger"></expenses-tracker-delete-pocketbook-graphic>
+        <p class="text-3xl text-et-color-fg-danger">Delete Pocketbook</p>
       </div>
       <div class="">
-        <p class="text-et-text">Are you sure you want to delete the pocketbook?</p>
+        <p class="">Are you sure you want to delete the pocketbook?</p>
       </div>
       <div class="flex flex-col">
         <div class="flex items-center justify-center gap-6">
@@ -31,7 +32,7 @@ import { DeletePocketbookGraphicComponent } from '@expenses-tracker/shared/asset
             matRipple
             [mat-dialog-close]="true"
             type="button"
-            class="flex justify-center items-center w-full gap-2 px-6 py-3 text-center text-et-danger bg-et-danger-bg rounded-lg cursor-pointer font-bold">
+            class="flex justify-center items-center w-full gap-2 px-6 py-3 text-center text-et-color-button-danger bg-et-color-button-danger-bg rounded-lg cursor-pointer font-bold">
             <mat-icon class="min-w-[24px]">delete</mat-icon>
             <span>YES</span>
           </button>
@@ -39,7 +40,7 @@ import { DeletePocketbookGraphicComponent } from '@expenses-tracker/shared/asset
             matRipple
             mat-dialog-close
             type="button"
-            class="flex justify-center items-center w-full gap-2 px-6 py-3 text-center text-et-text rounded-lg cursor-pointer hover:underline hover:underline-offset-4 bg-et-text-bg hover:bg-et-text-bg-hover">
+            class="flex justify-center items-center w-full gap-2 px-6 py-3 text-center rounded-lg cursor-pointer hover:underline hover:underline-offset-4 text-et-color-button-default bg-et-color-button-default-bg hover:text-et-color-button-accent">
             <mat-icon class="min-w-[24px]">clear</mat-icon>
             <span>NO</span>
           </button>
