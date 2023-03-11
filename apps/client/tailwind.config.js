760e5c6ecdfe95f1552bbd48a4de1ac67153d2b4
Usama Ansari <usamazansari@outlook.com>
2023-03-11 17:01:37 +0100

    Revert "feat: completed dark theme implementation"
    This reverts commit 23046d66c546c12ca73516f5c03437e75655b760.

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,31 +1,13 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
 const { join } = require('path');
-const { blue, gray, green, red } = require('tailwindcss/colors');
-const plugin = require('tailwindcss/plugin');
+const { blue, red, green, gray } = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: [
-    plugin(({ addUtilities, theme }) => {
-      addUtilities({
-        '.gradient-light': {
-          '--tw-gradient-stops': `${theme('colors.green.100')}, ${theme(
-            'colors.cyan.100'
-          )}, ${theme('colors.indigo.100')}`
-        },
-        '.gradient-dark': {
-          '--tw-gradient-stops': `${theme('colors.green.900')}, ${theme(
-            'colors.cyan.900'
-          )}, ${theme('colors.indigo.900')}`
-        }
-      });
-    })
-  ],
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname)
   ],
-  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -34,11 +16,10 @@ module.exports = {
         danger: red[500],
         dark: gray[700],
         light: gray[100],
-        'border_color-light': gray[200],
-        'border_color-dark': gray[600],
         'disabled-dark': gray[200],
         'disabled-light': gray[400]
       }
     }
-  }
+  },
+  plugins: []
 };
