f35b42cd2f77ae3fce0b8036df12532960287176
Usama Ansari <98811057+usamazansari@users.noreply.github.com>
2023-06-11 13:34:56 +0200

    feat: migrate from `yarn` to `pnpm` (#27)
    * feat: migrate to pnpm
    
    * ci: migrate to pnpm
    
    * fix: disable deploy preview
    
    * fix: disable deploy preview
    
    * ci: fix uses issue
    
    * ci: fix on issue
    
    * chore: cleanup firebase hosting workflows
    
    * feat: add build on push workflow
    
    * fix: add firebase package to resolve errors
    
    * ci: add cache for installation
    
    * ci: add prod flag to pnpm installation
    
    * ci: add nx-set-shas
    
    * Revert "ci: add prod flag to pnpm installation"
    
    This reverts commit c2e012d495506503fdfb05cedee626defe91f2ac.
    
    * ci: resequence using Nx SHA
    
    * ci: disable SHA for now
    wip: implement SHA in main branch
    
    * ci: remove cache deps
    
    * chore: rename projects

--- libs/features/dashboard/jest.config.ts
+++ libs/features/dashboard/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'features-dashboard',
+  displayName: 'dashboard',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
