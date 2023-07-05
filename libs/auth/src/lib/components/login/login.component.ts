69b71a814671d763c36c9a68e79ec9c28265d83c
Usama Ansari <usamazansari@outlook.com>
2023-07-05 18:31:17 +0200

    feat: login component using custom UI

--- libs/auth/src/lib/components/login/login.component.ts
+++ libs/auth/src/lib/components/login/login.component.ts
@@ -1,20 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators
-} from '@angular/forms';
+import { Component, OnDestroy, OnInit, inject, signal } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { InputTextModule } from 'primeng/inputtext';
 
 import { ComponentFlags, LoginService } from './login.service';
 
 type LoginForm = {
-  email: FormControl<string | null>;
-  password: FormControl<string | null>;
+  email: FormControl<string>;
+  password: FormControl<string>;
+};
+
+type FromControlExtras = {
+  name: string;
+  value: string;
+  error: {
+    flag: boolean;
+    message: string;
+  };
 };
 
 @Component({
@@ -29,26 +32,68 @@ export class LoginComponent implements OnInit, OnDestroy {
   flags$!: Observable<ComponentFlags>;
   #login$!: Subscription;
 
-  constructor(private _fb: FormBuilder, private _service: LoginService) {}
+  #fb = inject(FormBuilder);
+  #service = inject(LoginService);
+
+  email = signal<FromControlExtras>({
+    name: 'email',
+    value: '',
+    error: {
+      flag: false,
+      message: ''
+    }
+  });
+
+  password = signal<FromControlExtras>({
+    name: 'password',
+    value: '',
+    error: {
+      flag: false,
+      message: ''
+    }
+  });
 
   ngOnInit() {
-    this.formGroup = this._fb.group<LoginForm>({
-      email: this._fb.control<string>('', {
+    this.formGroup = this.#fb.group<LoginForm>({
+      email: this.#fb.control<string>('', {
         validators: [Validators.required, Validators.email]
-      }),
-      password: this._fb.control<string>('', {
+      }) as FormControl<string>,
+      password: this.#fb.control<string>('', {
         validators: [Validators.required]
-      })
+      }) as FormControl<string>
     });
 
-    this._service.dismissError();
-    this.flags$ = this._service.watchFlags$();
+    this.formGroup.valueChanges.subscribe(() => {
+      this.email.update(props => ({
+        ...props,
+        value: this.formGroup.controls.email.value,
+        error: {
+          flag: this.getError(this.formGroup.controls.email),
+          message: this.getErrorMessage(props.name)
+        }
+      }));
+      this.password.update(props => ({
+        ...props,
+        value: this.formGroup.controls.password.value,
+        error: {
+          flag: this.getError(this.formGroup.controls.password),
+          message: this.getErrorMessage(props.name)
+        }
+      }));
+    });
+
+    this.#service.dismissError();
+    this.flags$ = this.#service.watchFlags$();
+  }
+
+  private getError(control: FormControl<string>): boolean {
+    return control.touched && !!control.errors;
   }
 
   login() {
     if (!this.formGroup.invalid) {
       const { email, password } = this.formGroup.value;
-      this.#login$ = this._service.login$({ email, password }).subscribe({
+      this.#login$ = this.#service.login$({ email, password }).subscribe({
         next: () => {
           this.formGroup.reset();
         }
@@ -57,21 +102,19 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
   dismissError() {
-    this._service.dismissError();
+    this.#service.dismissError();
   }
 
-  getError(formControlName = '') {
+  getErrorMessage(formControlName = '') {
     if (this.formGroup.get(formControlName)?.hasError('required')) {
-      return `${
-        formControlName.charAt(0).toUpperCase() + formControlName.slice(1)
-      } is required`;
+      return `${formControlName.charAt(0).toUpperCase() + formControlName.slice(1)} is required`;
     }
 
     if (this.formGroup.get(formControlName)?.hasError('email')) {
       return 'Invalid Email';
     }
 
-    return '';
+    return `Unknown validation error for ${formControlName.charAt(0).toUpperCase() + formControlName.slice(1)}`;
   }
 
   ngOnDestroy() {
