37ca337ba1d9cd971ec1ebb7105460280dee8456
Usama Ansari <usamazansari@outlook.com>
2023-07-09 17:16:43 +0200

    fix: add transparency to overlay plugin

--- libs/shared/common/src/lib/plugins/overlay.plugin.js
+++ libs/shared/common/src/lib/plugins/overlay.plugin.js
@@ -1,7 +1,7 @@
 module.exports = function ({ addComponents }) {
   const loadingOverlay = {
     '.et-loading-overlay': {
-      '@apply absolute top-0 bottom-0 left-0 right-0 z-10 bg-color-canvas-overlay': {}
+      '@apply absolute top-0 bottom-0 left-0 right-0 z-10 bg-color-neutral-muted': {}
     }
   };
 
