7392ee0af2759d178f2e87deca034f5373bfce83
Usama Ansari <usamazansari@outlook.com>
2023-07-09 17:20:45 +0200

    feat: update signup component with plugin UI

--- libs/auth/src/lib/components/signup/signup.component.ts
+++ libs/auth/src/lib/components/signup/signup.component.ts
@@ -1,19 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators
-} from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
+import { Component, OnDestroy, OnInit, inject, signal } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+
+import { FormGroupTypeGenerator, FromControlExtras } from '@expenses-tracker/shared/interfaces';
 
 import { ComponentFlags, SignupService } from './signup.service';
 
 type SignupForm = {
-  email: FormControl<string | null>;
-  password: FormControl<string | null>;
+  email: string;
+  password: string;
 };
 
 @Component({
@@ -23,29 +19,69 @@ type SignupForm = {
   templateUrl: './signup.component.html'
 })
 export class SignupComponent implements OnInit, OnDestroy {
-  formGroup!: FormGroup<SignupForm>;
+  formGroup!: FormGroup<FormGroupTypeGenerator<SignupForm>>;
+  error$ = new BehaviorSubject<string>('');
   flags$!: Observable<ComponentFlags>;
   #signup$!: Subscription;
-  #saveUser$!: Subscription;
 
-  constructor(private _fb: FormBuilder, private _service: SignupService) {}
+  #fb = inject(FormBuilder);
+  #service = inject(SignupService);
+
+  email = signal<FromControlExtras<SignupForm, 'email'>>({
+    name: 'email',
+    value: '',
+    error: {
+      flag: false,
+      message: ''
+    }
+  });
+
+  password = signal<FromControlExtras<SignupForm, 'password'>>({
+    name: 'password',
+    value: '',
+    error: {
+      flag: false,
+      message: ''
+    }
+  });
 
   ngOnInit() {
-    this.formGroup = this._fb.group<SignupForm>({
-      email: this._fb.control<string>('', {
+    this.formGroup = this.#fb.group<FormGroupTypeGenerator<SignupForm>>({
+      email: this.#fb.control<string>('', {
         validators: [Validators.required, Validators.email]
-      }),
-      password: this._fb.control<string>('', {
+      }) as FormControl<string>,
+      password: this.#fb.control<string>('', {
         validators: [Validators.required]
-      })
+      }) as FormControl<string>
     });
-    this.flags$ = this._service.watchFlags$();
+
+    this.formGroup.valueChanges.subscribe(() => {
+      this.email.update(props => ({
+        ...props,
+        value: this.formGroup.controls.email.value,
+        error: {
+          flag: this.getError(this.formGroup.controls.email),
+          message: this.getErrorMessage(props.name)
+        }
+      }));
+      this.password.update(props => ({
+        ...props,
+        value: this.formGroup.controls.password.value,
+        error: {
+          flag: this.getError(this.formGroup.controls.password),
+          message: this.getErrorMessage(props.name)
+        }
+      }));
+    });
+
+    this.#service.dismissError();
+    this.flags$ = this.#service.watchFlags$();
   }
 
   signup() {
     if (this.formGroup.valid) {
       const { email, password } = this.formGroup.value;
-      this.#signup$ = this._service.signup$({ email, password }).subscribe({
+      this.#signup$ = this.#service.signup$({ email, password }).subscribe({
         next: () => {
           this.formGroup.reset();
         }
@@ -53,26 +89,49 @@ export class SignupComponent implements OnInit, OnDestroy {
     }
   }
 
-  getError(formControlName = '') {
-    if (this.formGroup.get(formControlName)?.hasError('required')) {
-      return `${
-        formControlName.charAt(0).toUpperCase() + formControlName.slice(1)
-      } is required`;
+  private getError(control: FormControl<string>): boolean {
+    return control.touched && !!control.errors;
+  }
+
+  private getErrorMessage(formControlName: keyof SignupForm) {
+    if (this.formGroup.controls[formControlName]?.hasError('required')) {
+      return `${formControlName.charAt(0).toUpperCase() + formControlName.slice(1)} is required`;
     }
 
-    if (this.formGroup.get(formControlName)?.hasError('email')) {
+    if (this.formGroup.controls[formControlName]?.hasError('email')) {
       return 'Invalid Email';
     }
 
-    return '';
+    return `Unknown validation error for ${formControlName.charAt(0).toUpperCase() + formControlName.slice(1)}`;
   }
 
-  dismissError() {
-    this._service.dismissError();
+  checkControl(formControl: FromControlExtras<SignupForm, keyof SignupForm>) {
+    switch (formControl.name) {
+      case 'email':
+        this.email.update(props => ({
+          ...props,
+          error: {
+            flag: this.getError(this.formGroup.controls.email),
+            message: this.getErrorMessage(props.name)
+          }
+        }));
+        break;
+      case 'password':
+        this.password.update(props => ({
+          ...props,
+          error: {
+            flag: this.getError(this.formGroup.controls.email),
+            message: this.getErrorMessage(props.name)
+          }
+        }));
+        break;
+
+      default:
+        break;
+    }
   }
 
   ngOnDestroy() {
     this.#signup$?.unsubscribe();
-    this.#saveUser$?.unsubscribe();
   }
 }
