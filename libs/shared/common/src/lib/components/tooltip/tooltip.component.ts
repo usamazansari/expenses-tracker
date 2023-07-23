8127201d195de9db6800a5b2a69dc231fb7debe7
Usama Ansari <usamazansari@outlook.com>
2023-07-23 13:33:47 +0200

    feat: add tooltip position

--- libs/shared/common/src/lib/components/tooltip/tooltip.component.ts
+++ libs/shared/common/src/lib/components/tooltip/tooltip.component.ts
@@ -1,16 +1,33 @@
-import { Attribute, Component, EventEmitter, OnDestroy, Output, TemplateRef, ViewChild, signal } from '@angular/core';
+import { Attribute, Component, EventEmitter, Input, OnDestroy, Output, TemplateRef, ViewChild } from '@angular/core';
+
+export type TooltipPosition = 'above' | 'below' | 'left' | 'right';
 
 @Component({
   selector: 'expenses-tracker-tooltip',
   template: `
     <ng-template>
-      <div class="flex flex-col items-center max-w-xs" [ngClass]="classList()">
-        <div class="p-2 border rounded-md bg-color-canvas-overlay border-color-border-default">
+      <div
+        class="flex  items-center max-w-xs"
+        [ngClass]="{ classList: true, 'flex-col': ['above', 'below'].includes(tooltipPosition) }">
+        <div
+          class="p-2 border rounded-md bg-color-canvas-overlay border-color-border-default"
+          [ngClass]="{
+            'order-1': ['above', 'left'].includes(tooltipPosition),
+            'order-2': ['below', 'right'].includes(tooltipPosition)
+          }">
           <span [id]="id">
             <ng-content></ng-content>
           </span>
         </div>
-        <div class="tooltip-arrow bg-color-border-default w-5 h-5"></div>
+        <div
+          class="tooltip-arrow bg-color-border-default w-5 h-5"
+          [ngClass]="{
+            'order-1': ['below', 'right'].includes(tooltipPosition),
+            'order-2': ['above', 'left'].includes(tooltipPosition),
+            '-rotate-90': tooltipPosition === 'left',
+            'rotate-90': tooltipPosition === 'right',
+            'rotate-180': tooltipPosition === 'below'
+          }"></div>
       </div>
     </ng-template>
   `,
@@ -25,11 +42,12 @@ import { Attribute, Component, EventEmitter, OnDestroy, Output, TemplateRef, Vie
 export class TooltipComponent implements OnDestroy {
   @ViewChild(TemplateRef) tooltipTemplate!: TemplateRef<unknown>;
   id = '';
-  classList = signal('');
+  classList = '';
+  @Input() tooltipPosition: TooltipPosition = 'above';
   @Output() tooltipClose$ = new EventEmitter<void>();
 
   constructor(@Attribute('class') public _classList: string) {
-    this.classList.set(_classList);
+    this.classList = _classList;
   }
 
   ngOnDestroy() {
