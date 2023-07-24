2ebe2847107df04433d5e81d5ed12dfef7d28e13
Usama Ansari <usamazansari@outlook.com>
2023-07-24 18:16:06 +0200

    chore: remove comment

--- libs/shared/interfaces/src/lib/forms.ts
+++ libs/shared/interfaces/src/lib/forms.ts
@@ -4,7 +4,6 @@ type FormGroupTypeGenerator<T extends { [key: string]: unknown }> = {
   [K in keyof T]: FormControl<T[K]>;
 };
 
-// TODO: @usamazansari: rename this to `FormControlExtras`
 type FormControlExtras<T extends { [key: string]: unknown }, K extends keyof T> = {
   name: K;
   value: T[K];
