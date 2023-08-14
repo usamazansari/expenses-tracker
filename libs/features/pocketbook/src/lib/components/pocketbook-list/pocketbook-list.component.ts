f562f03e0d4ce6e71d064c07741b30f881e8e8b2
Usama Ansari <usamazansari@outlook.com>
2023-08-14 16:30:21 +0200

    feat: unify owner and collaborator pocketbook list

--- libs/features/pocketbook/src/lib/components/pocketbook-list/pocketbook-list.component.ts
+++ libs/features/pocketbook/src/lib/components/pocketbook-list/pocketbook-list.component.ts
@@ -1,26 +1,70 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
-
-import { PocketbookListService } from './pocketbook-list.service';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { Subject, debounceTime, distinctUntilChanged } from 'rxjs';
-import { PocketbookOwnerListComponent } from './pocketbook-owner-list/pocketbook-owner-list.component';
-import { PocketbookCollaboratorListComponent } from './pocketbook-collaborator-list/pocketbook-collaborator-list.component';
+
+import { INITIAL_FLAGS, IPocketbook } from '@expenses-tracker/shared/interfaces';
+
+import { PocketbookListItemComponent } from './pocketbook-list-item/pocketbook-list-item.component';
+import { ComponentFlags, PocketbookListService } from './pocketbook-list.service';
 
 @Component({
   selector: 'expenses-tracker-pocketbook-list',
   standalone: true,
-  imports: [CommonModule, PocketbookOwnerListComponent, PocketbookCollaboratorListComponent],
+  imports: [CommonModule, PocketbookListItemComponent],
   templateUrl: './pocketbook-list.component.html',
   styles: []
 })
 export class PocketbookListComponent implements OnInit {
   #searchText$ = new Subject<string>();
+  pocketbookList = signal<IPocketbook[]>([]);
   #service = inject(PocketbookListService);
+  flags = signal<ComponentFlags>({
+    pocketbookList: { ...INITIAL_FLAGS }
+  });
+  user = computed(() => this.#service.user());
+
+  constructor() {
+    toObservable(this.user).subscribe(() => {
+      this.fetchPocketbookList();
+    });
+  }
 
   ngOnInit() {
     this.#searchText$.pipe(debounceTime(250), distinctUntilChanged()).subscribe(searchText => {
       console.log({ searchText });
     });
+  }
+
+  private fetchPocketbookList() {
+    this.flags.update(value => ({
+      ...value,
+      pocketbookList: { ...value.pocketbookList, loading: true }
+    }));
+    this.#service.fetchPocketbookList().subscribe({
+      next: pocketbookList => {
+        this.flags.update(value => ({
+          ...value,
+          pocketbookList: { ...value.pocketbookList, loading: false, success: true, fail: false }
+        }));
+        this.pocketbookList.set(pocketbookList);
+      },
+      error: () => {
+        this.flags.update(value => ({
+          ...value,
+          pocketbookList: { ...value.pocketbookList, loading: false, success: false, fail: true }
+        }));
+        this.pocketbookList.set([]);
+      }
+    });
+  }
+
+  pocketbookListTrack(index: number, pocketbook: IPocketbook) {
+    return pocketbook.id;
+  }
+
+  isPocketbookOwner(pocketbook: IPocketbook) {
+    return pocketbook.owner === this.user()?.uid;
   }
 
   debounceSearch($: KeyboardEvent) {
--- libs/features/pocketbook/src/lib/components/pocketbook-list/pocketbook-list.service.ts
+++ libs/features/pocketbook/src/lib/components/pocketbook-list/pocketbook-list.service.ts
@@ -1,15 +1,27 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, computed, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { RoutePaths } from '@expenses-tracker/shared/common';
+import { ContextService, FirestoreService } from '@expenses-tracker/core';
+import { IFlag } from '@expenses-tracker/shared/interfaces';
 
-export type PocketbookViewMode = 'owner' | 'collaborator';
+export type ComponentFlags = {
+  pocketbookList: IFlag;
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class PocketbookListService {
   #router = inject(Router);
+  #context = inject(ContextService);
+  #firestore = inject(FirestoreService);
+
+  user = computed(() => this.#context.user());
+
+  fetchPocketbookList() {
+    return this.#firestore.watchPocketbookList$();
+  }
 
   gotoAddPocketbook() {
     this.#router.navigate([RoutePaths.Pocketbook, RoutePaths.EntityAdd]);
