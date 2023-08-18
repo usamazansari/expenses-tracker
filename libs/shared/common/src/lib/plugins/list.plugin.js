4ffb8999fb970e79a5711cafde165c3797e4684f
Usama Ansari <usamazansari@outlook.com>
2023-08-18 17:03:52 +0200

    style: improve list styles

--- libs/shared/common/src/lib/plugins/list.plugin.js
+++ libs/shared/common/src/lib/plugins/list.plugin.js
@@ -1,17 +1,23 @@
 module.exports = function ({ addComponents }) {
-  const listItem = {
-    '.et-list-item': {
+  const ulList = {
+    '.et-ul-list': {
+      '@apply p-2 my-0 list-none': {}
+    }
+  };
+  const ulListItem = {
+    '.et-ul-list-item': {
       '@apply relative p-2 pl-3 transition-colors duration-200 border-2 rounded-md cursor-pointer hover:bg-color-action-list-item-default-hover-bg lg:w-60 border-color-transparent':
         {}
     }
   };
 
-  const listItemActive = {
-    '.et-list-item-active': {
+  const ulListItemActive = {
+    '.et-ul-list-item-active': {
       '@apply bg-color-action-list-item-default-selected-bg border-l-color-primer-border-active': {}
     }
   };
 
-  addComponents(listItem);
-  addComponents(listItemActive);
+  addComponents(ulList);
+  addComponents(ulListItem);
+  addComponents(ulListItemActive);
 };
