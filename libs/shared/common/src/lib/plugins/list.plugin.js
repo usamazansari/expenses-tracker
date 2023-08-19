f585cf86adab8940189d426ae80fee53114a6d5c
Usama Ansari <usamazansari@outlook.com>
2023-08-19 21:16:02 +0200

    style: improve list ui

--- libs/shared/common/src/lib/plugins/list.plugin.js
+++ libs/shared/common/src/lib/plugins/list.plugin.js
@@ -7,14 +7,14 @@ module.exports = function ({ addComponents }) {
 
   const ulListItem = {
     '.et-ul-list-item': {
-      '@apply p-2 pl-3 transition-colors duration-200 border-2 rounded-md cursor-pointer border-color-transparent lg:w-60 text-color-fg-muted hover:bg-color-action-list-item-default-hover-bg hover:text-color-fg-default':
+      '@apply p-2 pl-3 transition-colors duration-200 border-l-4 rounded-md cursor-pointer border-color-transparent lg:w-60 text-color-fg-muted hover:bg-color-action-list-item-default-hover-bg hover:text-color-fg-default my-1':
         {}
     }
   };
 
   const ulListItemActive = {
     '.et-ul-list-item-active': {
-      '@apply bg-color-action-list-item-default-selected-bg border-l-color-primer-border-active text-color-fg-default':
+      '@apply bg-color-action-list-item-default-selected-bg border-l-4 border-l-color-primer-border-active text-color-fg-default':
         {}
     }
   };
