f89e1063e0db073dd268188a33635b923d33f35b
Usama Ansari <usamazansari@outlook.com>
2023-08-26 11:49:48 +0200

    chore: add todos

--- libs/features/transaction/src/lib/types/transaction-form.ts
+++ libs/features/transaction/src/lib/types/transaction-form.ts
@@ -3,8 +3,11 @@ import { PaymentMode, TransactionCategory, TransactionDirection } from '@expense
 export type TransactionForm = {
   amount: number;
   category: TransactionCategory;
+  // TODO: @usamazansari: change to transactionType
   direction: TransactionDirection;
+  // TODO: @usamazansari: change to description
   message: string;
   paymentMode: PaymentMode;
+  // TODO: @usamazansari: change to transactionDate
   timestamp: Date;
 };
