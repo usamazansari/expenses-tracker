155803bbf120a7e3080b14ffafb969fa0b06f286
Usama Ansari <usamazansari@outlook.com>
2023-09-09 19:15:06 +0200

    chore: implement flags

--- libs/features/transaction/src/lib/components/transaction-edit/transaction-edit.component.ts
+++ libs/features/transaction/src/lib/components/transaction-edit/transaction-edit.component.ts
@@ -6,17 +6,19 @@ import { TransactionDAO } from '@expenses-tracker/shared/interfaces';
 
 import { TransactionFormComponent } from '../transaction-form/transaction-form.component';
 import { TransactionEditService } from './transaction-edit.service';
+import { TooltipModule } from '@expenses-tracker/shared/common';
 
 @Component({
   selector: 'expenses-tracker-transaction-edit',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, TransactionFormComponent],
+  imports: [CommonModule, ReactiveFormsModule, TransactionFormComponent, TooltipModule],
   templateUrl: './transaction-edit.component.html'
 })
 export class TransactionEditComponent {
   #service = inject(TransactionEditService);
 
   transaction = computed(() => this.#service.transaction() as TransactionDAO);
+  flags = computed(() => this.#service.flags());
 
   gotoTransactionList() {
     this.#service.gotoTransactionList();
