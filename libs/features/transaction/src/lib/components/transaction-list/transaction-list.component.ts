c11d32e2b75031704d23d056e7722efe6c4d70e2
Usama Ansari <usamazansari@outlook.com>
2023-09-15 11:26:15 +0200

    fix: set view mode

--- libs/features/transaction/src/lib/components/transaction-list/transaction-list.component.ts
+++ libs/features/transaction/src/lib/components/transaction-list/transaction-list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { Component, OnInit, computed, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Subject, debounceTime, distinctUntilChanged } from 'rxjs';
 
@@ -29,17 +29,18 @@ export class TransactionListComponent implements OnInit {
   transactionList = computed(() => this.#service.transactionList());
   pocketbook = computed(() => this.#service.pocketbook());
   flags = computed(() => this.#service.flags().transactionList);
+  viewMode = computed(() => (this.#service.transactionListViewMode() as TransactionListViewTypes) ?? 'monthly');
   viewOptions: SegmentedControlWrapper<TransactionListViewTypes>[] = [
     { icon: 'calendar_view_month', tooltip: 'Monthly view', value: 'monthly' },
     { icon: 'calendar_view_week', tooltip: 'Weekly view', value: 'weekly' },
     { icon: 'calendar_view_day', tooltip: 'Daily view', value: 'daily' }
   ];
-  viewMode = signal<TransactionListViewTypes>('monthly');
 
   ngOnInit() {
     this.#searchText$.pipe(debounceTime(250), distinctUntilChanged()).subscribe(searchText => {
       console.log({ searchText });
     });
+    this.#service.gotoViewMode(this.viewMode());
   }
 
   addTransaction() {
@@ -54,7 +55,7 @@ export class TransactionListComponent implements OnInit {
     this.#searchText$.next(($.target as HTMLInputElement).value);
   }
 
-  changeViewMode(value: TransactionListViewTypes) {
-    this.viewMode.set(value);
+  changeViewMode(transactionListViewMode: TransactionListViewTypes) {
+    this.#service.gotoViewMode(transactionListViewMode);
   }
 }
